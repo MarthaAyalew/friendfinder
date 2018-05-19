9a16c1b04f8612734fe1133ab6160e9ba60a732e
Martha Ayalew <Marti@Marthas-MacBook-Pro.local>
2018-05-18 23:11:50 -0400

    updated friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,66 @@
+var friendsArray = [
+	{
+		name: 'Sid',
+		photo: 'https://media0.giphy.com/media/3o7TKUbwzVVKaDDJm0/giphy.gif',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+
+		]
+    },
+    
+	{
+		name: 'Sid again',
+		photo: 'https://i.pinimg.com/originals/b8/34/70/b834704365ef58b72915f9daea6cb83f.gif',
+		scores: [
+			'1',
+			'3',
+			'3',
+			'3',
+			'4',
+
+		]
+    },
+
+    {
+		name: 'baby',
+		photo: 'https://fsmedia.imgix.net/7b/2d/47/a4/a9cf/4409/b3f9/85758c6722f8/iceage04gif.gif',
+		scores: [
+			'1',
+			'5',
+			'2',
+			'1',
+			'4',
+
+		]
+    },
+
+    {
+		name: 'Crash',
+		photo: 'https://s-media-cache-ak0.pinimg.com/originals/5b/25/86/5b25862dc95e9c376ad3070de1bdcec3.gif',
+		scores: [
+			'3',
+			'4',
+			'2',
+			'3',
+			'5',
+
+		]
+    },
+    {
+		name: 'Ellie',
+		photo: 'https://data.whicdn.com/images/89541865/original.gif',
+		scores: [
+			'1',
+			'3',
+			'4',
+			'1',
+			'5',
+
+		]
+    },
+]
+module.exports = friendsArray;
